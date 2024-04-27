75fa29aa2616c3df7f26f5fd9b830fa4f4e23e09
MikeyPants <michael.a.hazan@gmail.com>
2024-04-27 19:21:22 +0300

    Tuple Update, Removed unnecessary function

--- src/main/java/com/mikeypants/thegoodbush/item/registries/CannabisRegistry.java
+++ src/main/java/com/mikeypants/thegoodbush/item/registries/CannabisRegistry.java
@@ -1,25 +1,20 @@
 package com.mikeypants.thegoodbush.item.registries;
 
-import com.mikeypants.thegoodbush.lib.tuples.ItemTuple3;
+import com.mikeypants.thegoodbush.lib.tuples.CannabisItemsTuple4;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemNameBlockItem;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.RegistryObject;
 
 import static com.mikeypants.thegoodbush.item.ModItems.SIMPLEITEMS;
 
 public class CannabisRegistry {
-    // None Empty Items
-    public static ItemTuple3 registerCannabisType(String name, Item seed, Item leaf, Item bud){
-        var a = SIMPLEITEMS.register(name+ "_cannabis_seed", () -> seed);
-        var b = SIMPLEITEMS.register(name + "_cannabis_leaf", () -> leaf);
-        var c = SIMPLEITEMS.register(name + "_cannabis_bud", () -> bud);
+    public static CannabisItemsTuple4 registerCannabisType(String name, RegistryObject<Block> block){
+        var seed = SIMPLEITEMS.register(name + "_cannabis_seeds", () -> new ItemNameBlockItem(block.get(), new Item.Properties()));
+        var leaf = SIMPLEITEMS.register(name + "_cannabis_leaf", () -> new Item(new Item.Properties()));
+        var bud = SIMPLEITEMS.register(name + "_cannabis_bud", () -> new Item(new Item.Properties()));
+        var stem = SIMPLEITEMS.register(name + "_cannabis_stem", () -> new Item(new Item.Properties()));
 
-        return new ItemTuple3(a,b,c);
-    }
-    // Empty Items
-    public static ItemTuple3 registerCannabisType(String name){
-        var a = SIMPLEITEMS.register(name+ "_cannabis_seeds", () -> new Item(new Item.Properties()));
-        var b = SIMPLEITEMS.register(name + "_cannabis_leaf", () -> new Item(new Item.Properties()));
-        var c = SIMPLEITEMS.register(name + "_cannabis_bud", () -> new Item(new Item.Properties()));
-
-        return new ItemTuple3(a,b,c);
+        return new CannabisItemsTuple4(seed, leaf, bud, stem);
     }
 }
